6b900c68f0ff26afb4ffd1e19de4e45f1bc54c3e
Ekrem TAŞ <ekremtas589@gmail.com>
2020-06-17 12:22:59 +0300

    Updates

---
+++ precache-manifest.af05ffb279e7f7087251bd5a45bc00bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401c5f2af962cea565d7fe9932d45a83",
+    "url": "/dogs-app/index.html"
+  },
+  {
+    "revision": "600902ef2283aeb2feda",
+    "url": "/dogs-app/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "87c3471e2aa32f8973f5",
+    "url": "/dogs-app/static/css/main.985fe39e.chunk.css"
+  },
+  {
+    "revision": "600902ef2283aeb2feda",
+    "url": "/dogs-app/static/js/2.1a08f208.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dogs-app/static/js/2.1a08f208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c3471e2aa32f8973f5",
+    "url": "/dogs-app/static/js/main.58af769f.chunk.js"
+  },
+  {
+    "revision": "d0efefb9853fd1d41a22",
+    "url": "/dogs-app/static/js/runtime-main.e0bcfdbe.js"
+  }
+]);
